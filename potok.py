6851aa9356a8a06e81992b414934c05ec7178102
Alexandr Navrotskyi <ledeventsmasters@gmail.com>
2022-05-30 01:44:27 +0300

    another try

--- potok.py
+++ potok.py
@@ -3,8 +3,14 @@ if user_age <= 13:
     print('детство')
 elif 14 <= user_age <= 24:
     print('молодость')
-elif 25 <= user_age <= 59:
+elif 25 > user_age or user_age > 59:
     print('зрелость')
 else:
     print('старость')
 
+#
+#
+#
+#
+
+# a, b, c = int(input(':')), int(input(':')), int(input(':'))
