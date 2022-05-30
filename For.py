a6be5d091478dd90c119cc4d352f02919bc31bda
Alexandr Navrotskyi <ledeventsmasters@gmail.com>
2022-05-30 16:37:41 +0300

    another try

---
+++ For.py
@@ -0,0 +1,13 @@
+text = input('Enter text:')
+
+for character in text:
+    print(character, end='\n')
+print()
+
+number = input('enter number:')
+
+for digit in number:
+    print(int(digit)**2, end='')
+print()
+
+
---
+++ while.py
@@ -0,0 +1,8 @@
+num = int(input("enter number:"))
+numbers_count = 0
+numbers_max = 10
+
+while num < 20 and numbers_count < numbers_max:
+    num += 1
+    numbers_count += 1
+    print(num)
